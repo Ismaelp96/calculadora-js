7cd1e8c981a6c1cb1928b4d88e894d6e726503ac
Ismael Patrick <ismael.patrick@belmicro.com.br>
2022-11-17 16:18:35 -0300

    Adcionando switch nos botões

--- js/scripts/controller/CalController.js
+++ js/scripts/controller/CalController.js
@@ -1,80 +1,138 @@
 class CalcController {
   constructor() {
-    this._locale = "pt-BR";
-    this._displayCalcEl = document.querySelector("#display");
-    this._dateEl = document.querySelector("#data");
-    this._timeEl = document.querySelector("#hora");
-    this._currentDate;
-    this.initialize();
-    this.initButtonsEvents();
+    this._operation = []
+    this._locale = 'pt-BR'
+    this._displayCalcEl = document.querySelector('#display')
+    this._dateEl = document.querySelector('#data')
+    this._timeEl = document.querySelector('#hora')
+    this._currentDate
+    this.initialize()
+    this.initButtonsEvents()
   }
 
   setDisplayDateTime() {
     this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
-      day: "2-digit",
-      month: "long",
-      year: "numeric",
-    });
-    this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    })
+    this.displayTime = this.currentDate.toLocaleTimeString(this._locale)
   }
 
   initialize() {
-    this.setDisplayDateTime();
+    this.setDisplayDateTime()
 
     setInterval(() => {
-      this.setDisplayDateTime();
-    }, 1000);
+      this.setDisplayDateTime()
+    }, 1000)
   }
 
   addEventListenerAll(element, events, fn) {
-    events.split(" ").forEach((event) => {
-      element.addEventListener(event, fn, false);
-    });
+    events.split(' ').forEach(event => {
+      element.addEventListener(event, fn, false)
+    })
+  }
+  clearAll() {
+    this._operation = []
+  }
+
+  clearEntry() {
+    this._operation.pop()
+  }
+
+  addOperation(value) {
+    this._operation.push(value)
+
+    console.log(this._operation)
+  }
+
+  setError() {
+    this.displayCalc = 'Error'
+  }
+  execBtn(value) {
+    switch (value) {
+      case 'ac':
+        this.clearAll()
+        break
+      case 'ce':
+        this.clearEntry()
+        break
+      case 'soma':
+        break
+      case 'subtracao':
+        break
+      case 'divisao':
+        break
+      case 'multiplicacao':
+        break
+      case 'porcento':
+        break
+      case 'igual':
+        break
+
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        this.addOperation(parseInt(value))
+        break
+
+      default:
+        this.setError()
+        break
+    }
   }
 
   initButtonsEvents() {
-    let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+    let buttons = document.querySelectorAll('#buttons > g, #parts > g')
 
     buttons.forEach((btn, index) => {
-      this.addEventListenerAll(btn, "click drag", () => {
-        console.log(btn.className.baseVal.replace("btn-", ""));
-      });
+      this.addEventListenerAll(btn, 'click drag', () => {
+        let textBtn = btn.className.baseVal.replace('btn-', '')
+        this.execBtn(textBtn)
+      })
 
-      this.addEventListenerAll(btn, "mouseover mouseup mousedown", () => {
-        btn.style.cursor = "pointer";
-      });
-    });
+      this.addEventListenerAll(btn, 'mouseover mouseup mousedown', () => {
+        btn.style.cursor = 'pointer'
+      })
+    })
   }
 
   // time
   get displayTime() {
-    return this._timeEl.innerHTML;
+    return this._timeEl.innerHTML
   }
 
   set displayTime(time) {
-    this._timeEl.innerHTML = time;
+    this._timeEl.innerHTML = time
   }
 
   // date
   get displayDate() {
-    return this._dateEl.innerHTML;
+    return this._dateEl.innerHTML
   }
 
   set displayDate(date) {
-    return (this._dateEl.innerHTML = date);
+    return (this._dateEl.innerHTML = date)
   }
 
   get displayCalc() {
-    return this._displayCalcEl.innerHTML;
+    return this._displayCalcEl.innerHTML
   }
   set displayCalc(value) {
-    this._displayCalcEl.innerHTML = value;
+    this._displayCalcEl.innerHTML = value
   }
 
   get currentDate() {
-    return new Date();
+    return new Date()
   }
   set currentDate(data) {
-    this._currentDate = data;
+    this._currentDate = data
   }
 }
